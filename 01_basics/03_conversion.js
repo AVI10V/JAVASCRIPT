2292d9374444a4765300c4c1cee4ab296e4c0abc
AVI10V <119165939+AVI10V@users.noreply.github.com>
2024-09-07 18:46:14 +0000

    lec-6 conversion

---
+++ 01_basics/03_conversion.js
@@ -0,0 +1,19 @@
+let score="33";
+let ss="33abc"
+console.log(typeof score)
+let number=Number(score)
+console.log(typeof number)
+let nos=Number(ss)
+console.log(typeof ss)
+console.log(ss)
+let nn=1
+let ans=Boolean(nn)
+console.log(typeof ans)
+let someno = 33;
+let snos=String(someno)
+console.log(typeof snos)
+//'33'--33
+// true -- 1
+//"33abc"--nan
+// empty string  gives false else gives true (string)
+
