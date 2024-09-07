063fd0d1525543670c92c1456b223392f48df79d
AVI10V <119165939+AVI10V@users.noreply.github.com>
2024-09-07 18:29:53 +0000

    lect-5

---
+++ data_types.js
@@ -0,0 +1,15 @@
+"use strict;" //to use newer version oof nodejs
+//alert(3+3) // cant use we are workin on nodejsnot on browser
+let name="avi"
+let nos=98;
+let bool= true;
+// number 2 to the power 53
+//bigint
+//string '' or ""
+//boolean
+//undefined not defined for var
+//null rep of empty not undefined but define null
+// symbol unique
+//object
+console.log(typeof null); //gives objects
+//undefined type of is undefined remember 
