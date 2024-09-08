539f49857a514d912e655e73a82c7827a1aab919
AVI10V <119165939+AVI10V@users.noreply.github.com>
2024-09-08 06:09:13 +0000

    strings basics

---
+++ 01_basics/string.js
@@ -0,0 +1,15 @@
+const name="avi"
+const age=23
+console.log(`Hello my name is ${name} and my age is ${age}`)//using bacticks allows functionality in variables to be added using.
+//another way of defining a string allows tov vuse various methods
+const myname= new String("    avishva")
+console.log(myname.length)
+console.log(myname.charAt(4))
+console.log(myname[1])
+console.log(myname.indexOf('a'))
+console.log(myname.lastIndexOf('a'))
+console.log(myname.replace('a','j'))
+console.log(name.length)
+console.log(name.substring(1))//can also use it with normalconst type
+console.log(myname.t)
+console.log(myname)
