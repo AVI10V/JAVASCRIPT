e0d1b625c6565ed3752e089c1014962cd1623c5b
AVI10V <119165939+AVI10V@users.noreply.github.com>
2024-09-08 08:58:48 +0000

    maths and number

---
+++ 01_basics/mathsandnumber.js
@@ -0,0 +1,17 @@
+const score=10;
+// const number= new Number(100000000)
+// console.log(number)
+// console.log(typeof number) //object
+// console.log(number.toPrecision(6))
+// console.log(number.toLocaleString())
+
+// MATH
+// console.log(Math.abs(-6))
+// console.log(Math.floor(1.999))
+// console.log((Math.floor(Math.random()*6))+1)
+
+// to get from in to max 
+
+const min=10;
+const max=20;
+console.log(Math.floor(Math.random()*(max-min+1))+min)
