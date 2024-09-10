cda0ed36053bd7d8e5ae10705d5be96817f949b1
AVI10V <119165939+AVI10V@users.noreply.github.com>
2024-09-10 11:17:42 +0000

    array

---
+++ 02_basics/array1.js
@@ -0,0 +1,27 @@
+//ARRAY
+const myarray=[1,2,3,4] //object of multiple types is js are resizeable
+// console.log(myarray[3]);
+// const her=["avi","hero"]
+// const myarr= new Array(1,2,3,4)
+myarray.push(9)
+// console.log(myarray)
+
+// shift unshift 
+//unshift adds the given number at 1st position and shifts the array by one
+
+// myarray.unshift("sex")
+// console.log(myarray)
+// myarray.shift()
+// console.log(myarray)
+
+
+
+// slice vs splice
+
+//slice just slices from initial index to last exclusive
+//splice slices but inclusive of last and ********** and also makes changes to orignal array removes those elements spliced 
+console.log(myarray.slice(1,3))
+console.log(myarray)
+
+console.log(myarray.splice(1,3))
+console.log(myarray)
